55eff13254c02c84d5e05b46a14cc571a5874065
Kamil Kwiatkowski <kaczy.kwiatkowski@gmail.com>
2020-12-08 11:11:43 +0100

    pr

--- portfolio (5)/portfolio/js/script.js
+++ portfolio (5)/portfolio/js/script.js
@@ -1,22 +1,11 @@
 const showMenu = function showMenu() {
-  const menuTrigger = document.querySelector(".icon-menu");
+  const menuTrigger = document.querySelector("nav");
   const menuNav = document.querySelector("#menu_nav");
-  const bodyHandler = document.querySelector("body");
-
   menuTrigger.onclick = () => {
     menuNav.classList.toggle("open");
-    bodyHandler.classList.toggle("open_menu");
-  };}
+  };
+};
 
-
-  
-  $(window).bind("resize", function () {
-    if (window.innerWidth < 757) {
-    showMenu()
-    ;};});
-
- 
-  
-
-
-
+if (window.innerWidth < 757) {
+  showMenu();
+}
