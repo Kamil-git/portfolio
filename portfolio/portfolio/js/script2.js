43366aa064d1e7c568baaeba21c338da826bad9c
Kamil Kwiatkowski <kaczy.kwiatkowski@gmail.com>
2020-12-11 12:10:16 +0100

    ok

---
+++ portfolio/portfolio/js/script2.js
@@ -0,0 +1,8 @@
+$('a.scroll').click(function(){
+  var href = $(this).attr('href');
+  var dest = $(href).offset().top;
+  $('html, body').animate({
+    scrollTop: dest;
+  }, 1000);
+  return false;
+});
